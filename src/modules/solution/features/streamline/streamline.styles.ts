9078ea2eff8b7e2bc29b340131c5147e0aedcecf
Yamazaki Araki <yamazaki.softdev@gmail.com>
2023-09-01 19:49:17 +0800

    update: image rendering

--- src/modules/solution/features/streamline/streamline.styles.ts
+++ src/modules/solution/features/streamline/streamline.styles.ts
@@ -51,6 +51,7 @@ export const StreamlineImageWrapper = styled.div`
     /* padding-top: 50px; */
     img {
       width: 100%;
+      image-rendering: pixelated;
     }
   }
   @media screen and (max-width: 768px) {
